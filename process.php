b98edb2e8efbbc11ac84b43216a0edb0ba499f83
David Nuon <david@davidnuon.com>
2014-12-10 21:28:51 +0000

    Editing

--- process.php
+++ process.php
@@ -1,33 +1,20 @@
 <?php
-//Saves messages file to variable
 $guestbook = "messages.txt";
 
-// This runs if some sends any user input
-if(isset($_POST['button'])){
-    if(!empty($_POST['name'])){
 
-        $name = $_POST['name']."\n";
-        $msg = $_POST['message']."\n";
-        $lang = $_POST['language']."\n<hr>";
-        global $name, $msg, $lang;
-        $file = fopen($guestbook,"a");
-        fwrite($file, "$name $msg $lang");
-        fclose($file);
 
-    }else{
-        echo '<script>alert("Please enter your name")</script>';}
-}
-$readfile = fopen($guestbook, "r");
-// Read and print out everything in the page.
-echo @fread($readfile, filesize($guestbook));
-// Close the file we opened.
-fclose($readfile);
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 if($method === 'POST')
 {
-    header('Content-Type: application/json');
-    write_me();
+    if(isset($_POST['method'])) {
+	$method = $_POST['method'];
+	if($method === 'write') {
+	    header('Content-Type: application/json');
+	    write_me();
+	}
+    }
 
 } else if($method === 'GET') {
     header('Content-Type: application/json');
@@ -54,8 +41,7 @@ function get_latest() {
 }
 
 function write_me() {
-    var_dump($_POST);
-    echo '';
+    echo json_encode($_POST);
 }
 
 

953b4789d8ad0ebebfd40010ee8028f96427f8ea
David Nuon <david@davidnuon.com>
2014-12-14 13:42:12 -0800

    Start to refactor

--- process.php
+++ process.php
@@ -1,26 +1,8 @@
 <?php
+
 $guestbook = "messages.txt";
-
-
-
-
-
-$method = $_SERVER['REQUEST_METHOD'];
-if($method === 'POST')
-{
-    if(isset($_POST['method'])) {
-	$method = $_POST['method'];
-	if($method === 'write') {
-	    header('Content-Type: application/json');
-	    write_me();
-	}
-    }
-
-} else if($method === 'GET') {
-    header('Content-Type: application/json');
-    get_latest();
-    get_all();
-}
+$method     = $_SERVER['REQUEST_METHOD'];
+header('Content-Type: application/json');
 
 function get_all() {
    // var_dump($_GET);
@@ -30,7 +12,6 @@ function get_all() {
                         "Language" => $_POST['language']);
     echo json_encode($alldata);
 }
-
 
 function get_latest() {
     $latestpost = array("Name" => $_POST['name'],
@@ -44,8 +25,27 @@ function write_me() {
     echo json_encode($_POST);
 }
 
-
-
+if($method === 'POST') {
+    $methodName = isset($_POST['method']) ? $_POST['method'] : null;
+    switch ($methodName) {
+        case 'write':
+	        write_me();    
+            break;
+        default:
+            break;
+    }
+} else if($method === 'GET') {
+    $methodName = isset($_GET['method']) ? $_GET['method'] : null;
+    switch ($methodName) {
+        case 'latest':
+            get_latest();
+            break;
+        default:
+        case 'all':
+            get_all();
+            break;
+    }
+}
 
 ?>
 
